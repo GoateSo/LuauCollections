4e88fbff13ce981ee97fb42310abfffc95087a04
GoateSo <59705775+GoateSo@users.noreply.github.com>
2020-11-29 19:24:00 -0600

    Add documentation
    documentation style changed to nominally follow luaDoc style

--- Utilities/Option.lua
+++ Utilities/Option.lua
@@ -2,29 +2,12 @@ local Option = {}
 Option.__index = Option
 setmetatable(Option,Option)
 local None = setmetatable({},Option)
+---Constructs an Option from a given value, giving a nil will become a "None", and giving any other value becomes that value wrapped within the context of Option
 --[[
-Constructs an Option from a given value, giving a nil will become a "None", and giving any other value becomes that value wrapped within the context of Option
 @params
-	v:A
-		value to be wrapped
+	v:A = value to be wrapped
 @return
-	Option[A]
-		value wrapped in context of Option
-]]
-function Option:__call(v)
-	if v ~= nil then
-		return setmetatable({get = v},Option)
-	end
-	return None
-end
---[[
-same functionality as above, provides more traditional "Object.new(...)" syntax
-@params
-	v:A
-		value to be wrapped
-@return
-	Option[A]
-		value wrapped in context of Option
+	Option[A] = value wrapped in context of Option
 ]]
 function Option.new(v)
 	if v ~= nil then
@@ -32,14 +15,12 @@ function Option.new(v)
 	end
 	return None
 end
+---returns an alternative Option if empty, otherwise returns itself
 --[[
-returns an alternative Option if empty, otherwise returns itself
 @params
-	v:Option[A]
-		alternative Option
+	v:Option[A] = alternative Option
 @return
-	Option[A]
-		resulting Option
+	Option[A] = resulting Option
 ]]
 function Option:orElse(v)
 	if self == None then 
@@ -47,14 +28,12 @@ function Option:orElse(v)
 	end
 	return self
 end
+---returns an alternative Option if empty, otherwise returns itself
 --[[
-returns an alternative Option if empty, otherwise returns itself
 @params
-	v:A
-		alternative value
+	v:A = alternative value
 @return
-	Option[A]
-		resulting Option
+	Option[A] = resulting Option
 ]]
 function Option:getOrElse(v)
 	if self == None then 
@@ -62,40 +41,34 @@ function Option:getOrElse(v)
 	end
 	return self.get
 end
+---Applies a function on the Optional value that returns an Option
 --[[
-Applies a function on the Optional value that returns an Option
 @params
-	f:A=>Option[B]
-		function to be applied
+	f:A=>Option[B] = function to be applied
 @return
-	Option[B]
-		resulting Option
+	Option[B] = resulting Option
 ]]
 function Option:flatMap(f)
 	if self == None then return self 
 	else return f(self.get) end
 end
+---Applies a function on the Optional value
 --[[
-Applies a function on the Optional value
 @params
-	f:A=>B
-		function to be applied
+	f:A=>B = function to be applied
 @return
-	Option[B]
-		resulting Option
+	Option[B] = resulting Option
 ]]
 function Option:map(f)
 	if self == None then return self 
 	else return Option.new(self.get) end
 end
+---checks if an Option fulfills a given predicate, if not None is returned
 --[[
-checks if an Option fulfills a given predicate, if not None is returned
 @params
-	p:A=>Boolean
-		predicate in question
+	p:A=>Boolean = predicate in question
 @return
-	Option[A]
-		resulting Option
+	Option[A] = resulting Option
 ]]
 function Option:filter(p)
 	if self == None then return self 
@@ -107,58 +80,48 @@ function Option:filter(p)
 		end
 	end
 end
+---turns the option into a string None -> "None", and anything else to "Some(...)"
 --[[
-turns the option into a string None -> "None", and anything else to "Some(...)"
-@params
 @return
-	String
-		resulting string
+	String = resulting string
 ]]
 function Option:__tostring()
 	if self == None then return "None"
 	else return "Some("..tostring(self.get)..")"end
 end
+---checks whether a Option is empty (=None)
 --[[
-checks whether a Option is empty (=None)
-@params
 @return
-	Boolean
-		whether the Option is empty or not
+	Boolean = whether the Option is empty or not
 ]]
 function Option:isEmpty()
 	return self==None
 end
+---checks whether a Option isn't None
 --[[
-checks whether a Option isn't None
-@params
 @return
-	Boolean
-		whether an Option isnt None
+	Boolean = whether an Option isnt None
 ]]
 function Option:isNonEmpty()
 	return self~=None
 end
+---checkers whether an Option contains a value
 --[[
-checkers whether an Option contains a value
 @params
-	v:A
-		value in question
+	v:A = value in question
 @return
-	Boolean
-		whether it contains that value
+	Boolean = whether it contains that value
 ]]
 function Option:contains(v)
 	if self == None then return false
 	else return self.get == v end
 end
+---checks value and type equality for Options
 --[[
-checks value and type equality for Options
 @params
-	that:Option[A]
-		value to be compared to
+	that:Option[A] = value to be compared to
 @return
-	Boolean
-		Whether they are equivalent
+	Boolean = Whether they are equivalent
 ]]
 function Option:__eq(that)
 	local a,b = rawequal(that,None),rawequal(self,None)
@@ -169,4 +132,4 @@ function Option:__eq(that)
 	end
 end
 
-return {Option,None}
+return {Option.new,Option,None}
